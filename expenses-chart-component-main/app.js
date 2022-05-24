ee288f94dfa16c18359fba050d377b708b66f0a8
Toni <99742252+Tonicc00@users.noreply.github.com>
2022-05-25 00:02:03 +0200

    Add files via upload

---
+++ expenses-chart-component-main/app.js
@@ -0,0 +1,84 @@
+const labels = [
+    'mon',
+    'tue',
+    'wed',
+    'thu',
+    'fri',
+    'sat',
+    'sun'
+];
+
+const data = {
+    labels : labels,
+    datasets : [{
+        label: '',
+        borderRadius: 5,
+        backgroundColor: ['hsl(10, 79%, 65%)','hsl(10, 79%, 65%)','hsl(186, 34%, 60%)','hsl(10, 79%, 65%)','hsl(10, 79%, 65%)','hsl(10, 79%, 65%)'],
+        data: [17.45, 34.91, 52.36, 31.07, 23.39, 43.28, 25.48]
+    }]
+};
+
+const config = {
+    type: 'bar',
+    data: data,
+    options: {
+        animation:{
+            dration: 3000
+        },
+        scales: {
+            x: {
+                grid: {
+                    display: false,
+                    drawBorder: false
+                },
+                ticks: {
+                    font: {
+                        family: 'DM Sans',
+                        size: 14,
+                    },
+                    color: 'hsl(28, 10%, 53%)'
+                },
+
+            },
+            y: {
+                display: false,
+            }
+        },
+        plugins: {
+            legend: {
+                display: false,
+            },
+            tooltip: {
+                callbacks: {
+                    title : () => null,
+                    label: 
+                    function(context) {
+                        let label = context.dataset.label || '';
+                        if (label) {
+                            label += ': ';
+                        }
+                        if (context.parsed.y !== null){
+                            label += new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'}).format(context.parsed.y);
+                        }
+                        return label
+                    }
+                }
+            }
+        }
+    }
+};
+
+Chart.defaults.font.family = "DM Sans"
+Chart.defaults.plugins.tooltip.padding = 10
+Chart.defaults.plugins.tooltip.displayColors = false
+Chart.defaults.plugins.tooltip.backgroundColor = 'hsl(25, 47%, 15%)'
+Chart.defaults.plugins.tooltipcornerRadius = 5
+Chart.defaults.plugins.tooltip.xAlign = 'center'
+Chart.defaults.plugins.tooltip.yAlign = 'bottom'
+Chart.defaults.plugins.tooltip.bodyAlign = 'center'
+Chart.defaults.plugins.tooltip.titleAlign = 'center'
+
+const myChart = new Chart(
+    document.getElementById('myChart').getContext('2d'),
+    config
+);
